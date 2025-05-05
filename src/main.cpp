fbb2d991f0faae95c2950d7564a0c97daf08e81b
Gtxripmxt <gtxripmxt@gmail.com>
2025-05-05 16:04:43 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,37 +5,37 @@
 using namespace geode::prelude;
 
 class $modify(ConfirmRestart, PauseLayer) {
-    CCMenuItemToggler* m_checkbox = nullptr;
-
-    bool init(bool fromReplay) {
-        if (!PauseLayer::init(fromReplay)) return false;
+    void customSetup() {
+        PauseLayer::customSetup(); // Call original first
 
         auto checkboxOff = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
         auto checkboxOn = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
 
-        m_checkbox = CCMenuItemToggler::create(
+        auto checkbox = CCMenuItemToggler::create(
             checkboxOff,
             checkboxOn,
             this,
             menu_selector(ConfirmRestart::onCheckbox)
         );
-        m_checkbox->setPosition({ 25.f, 25.f }); // Bottom-left corner
-        m_checkbox->setAnchorPoint({ 0.f, 0.f });
+        checkbox->setPosition({ 25.f, 25.f });
+        checkbox->setAnchorPoint({ 0.f, 0.f });
 
         auto menu = CCMenu::create();
-        menu->addChild(m_checkbox);
+        menu->addChild(checkbox);
         menu->setPosition({ 0.f, 0.f });
         this->addChild(menu);
 
-        return true;
+        // Save checkbox in Fields
+        setField<"checkbox">(checkbox);
     }
 
     void onCheckbox(CCObject*) {
-        // Optional: logic when checkbox toggled
+        // Handle toggle if needed
     }
 
-    void onRestart(CCObject* sender) override {
-        if (m_checkbox && m_checkbox->isOn()) {
+    void onRestart(CCObject* sender) {
+        auto checkbox = getField<"checkbox">();
+        if (checkbox && checkbox->isOn()) {
             geode::createQuickPopup(
                 "Restart",
                 "Are you sure you want to restart?",
@@ -56,4 +56,8 @@ class $modify(ConfirmRestart, PauseLayer) {
             }
         }
     }
+
+    struct Fields {
+        CCMenuItemToggler* checkbox = nullptr;
+    };
 };
