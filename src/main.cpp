cf100047e9dae39c35075de64876d2ab208f9929
Gtxripmxt <gtxripmxt@gmail.com>
2025-05-06 14:50:42 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,54 +12,69 @@ class $modify(ConfirmRestart, PauseLayer) {
     void customSetup() {
         PauseLayer::customSetup();
 
+        // Create checkbox visuals
         auto checkboxOff = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
         auto checkboxOn = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
 
-        bool saved = Mod::get()->getSavedValue<bool>("confirm-reset", true);      
-        
+        // Create toggler
         auto checkbox = CCMenuItemToggler::create(
             checkboxOff,
             checkboxOn,
             this,
             menu_selector(ConfirmRestart::onCheckbox)
         );
-        m_fields->checkbox = checkbox;
         checkbox->setPosition({ 25.f, 25.f });
         checkbox->setAnchorPoint({ 0.f, 0.f });
-        if (saved) {
-            checkbox->toggle(true);
+
+        // Set toggle state from saved value
+        bool saved = Mod::get()->getSavedValue("confirm-reset", true);
+        if (saved != checkbox->isOn()) {
+            checkbox->toggle(true); // toggle only if needed
         }
+
+        m_fields->checkbox = checkbox;
+
+        // Add to menu
         auto menu = CCMenu::create();
         menu->addChild(checkbox);
         menu->setPosition({ 0.f, 0.f });
         this->addChild(menu);
+
+        // Hook Restart button
+        if (auto restartBtn = static_cast<CCMenuItemSpriteExtra*>(this->getChildByIDRecursive("restart-button"))) {
+            restartBtn->setTarget(this, menu_selector(ConfirmRestart::onRestart));
+        } else {
+            log::warn("Failed to find restart button");
+        }
     }
 
     void onCheckbox(CCObject*) {
         if (m_fields->checkbox) {
             bool isChecked = m_fields->checkbox->isOn();
             Mod::get()->setSavedValue("confirm-reset", isChecked);
+            log::info("Checkbox toggled: {}", isChecked);
         }
     }
-      void onRestart(CCObject* sender) {
+
+    void onRestart(CCObject* sender) {
         bool shouldConfirm = Mod::get()->getSavedValue("confirm-reset", true);
-          if (shouldConfirm) {
-              geode::createQuickPopup(
+        if (shouldConfirm) {
+            geode::createQuickPopup(
                 "Restart",
                 "Are you sure you want to restart?",
                 "No", "Yes",
                 [this](auto, bool btn2) {
                     if (btn2) {
-                        if (auto playLayer = GameManager::sharedState()->getPlayLayer()) {
-                            playLayer->resetLevel();
+                        if (auto pl = GameManager::sharedState()->getPlayLayer()) {
+                            pl->resetLevel();
                             this->onResume(nullptr);
                         }
                     }
                 }
             );
         } else {
-            if (auto playLayer = GameManager::sharedState()->getPlayLayer()) {
-                playLayer->resetLevel();
+            if (auto pl = GameManager::sharedState()->getPlayLayer()) {
+                pl->resetLevel();
                 this->onResume(nullptr);
             }
         }
